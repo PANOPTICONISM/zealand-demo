c8dc2c207204eeb4ec3b086386a1b69985aa847e
panopticonism <panopticonism@gmail.com>
2022-12-01 17:54:08 +0100

    Tags added.

--- csr-react/src/components/Product/Product.tsx
+++ csr-react/src/components/Product/Product.tsx
@@ -4,6 +4,7 @@ import styles from "./styles.module.css";
 export const Product = ({ product }: { product: ProductProps }) => {
     return (
         <article>
+            <span>{product.category[0]}</span>
             <img src={product.image.url} alt={product.title} className={styles.image} />
             <h2>{product.title}</h2>
             <p className={styles.price}>{product.price} DKK</p>
