1cd7c917987716db597ab98660cbf2dc8ab27f88
panopticonism <panopticonism@gmail.com>
2022-12-01 17:12:39 +0100

    Products fetch + typed.

---
+++ csr-react/src/types/types.ts
@@ -0,0 +1,41 @@
+export type EntryProps = {
+    items: {
+        fields: {
+            title: string,
+            category: Array<string>,
+            price: string,
+            image: {
+                fields: {
+                    description: string,
+                    title: string,
+                    file: {
+                        url: string,
+                        fileName: string,
+                        contentType: string,
+                        details: {
+                            height: number,
+                            width: number,
+                            size: number,
+                        }
+                    }
+                }
+            }
+        },
+    }[]
+}
+
+export type ProductProps = {
+    title: string,
+    category: Array<string>,
+    price: string,
+    image: {
+        url: string,
+        fileName: string,
+        contentType: string,
+        details: {
+            height: number,
+            width: number,
+            size: number,
+        }
+    }
+}
